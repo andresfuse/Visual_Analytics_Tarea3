c2d49d4d4ebb08bd8c88dcdafdc403536291091b
andresfuse <42395274+andresfuse@users.noreply.github.com>
2018-09-25 08:18:28 -0500

    Update grafica.js

--- grafica.js
+++ grafica.js
@@ -137,23 +137,6 @@ var z = d3.scaleOrdinal()
          .transition().duration(800)
             .attr("y", function(d) { return y(d[1]); })
             .text(function(d) {return d.total;});
-    
-    var svg = d3.select("body").append("svg")
-        .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom)
-      .append("g")
-        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-    svg.append("g")
-        .attr("class", "x axis")
-        .attr("transform", "translate(0," + height + ")")
-        .call(xAxis)
-      .selectAll("text")
-        .attr("y", 0)
-        .attr("x", 9)
-        .attr("dy", ".35em")
-        .attr("transform", "rotate(90)")
-        .style("text-anchor", "start");
 
 }
 actualizar(data)
